8b99057540cc345e7caa013ed3ec6efc3f42b773
Ryan Florence <rpflorence@gmail.com>
2011-10-05 21:01:23 -0600

    Switched to node-tap for testing

--- test/tests.js
+++ test/tests.js
@@ -1,126 +1,134 @@
-var advise = (typeof window != 'undefined' ? advise : require('./advise.js'));
+var test = require('tap').test,
+    advise = require('../src/advise');
 
-function newObj (){
+console.log('blah!')
+
+function newObj () {
   return {
     arr: [],
-    push: function (val){
+    push: function (val) {
       this.arr.push(val);
     },
-    add: function (x, y){
+    add: function (x, y) {
       return x + y;
     },
-    foo: function (foo){
+    foo: function (foo) {
       return foo;
     }
   };
 }
 
-module('basics');
-test('before should', function (){
+test('before should', function (t) {
   var obj = newObj(),
       context;
 
-  var advice = advise(obj).before('push', function (){
+  var advice = advise(obj).before('push', function () {
     context = this;
     obj.arr.push('0');
   });
 
   obj.push('1');
-  equal(obj.arr[0], '0', 'call advice before the method');
-  equal(obj, context, 'set the context to the object');
+  t.equal(obj.arr[0], '0', 'call advice before the method');
+  t.equal(obj, context, 'set the context to the object');
 
   advice.detach();
   obj.push('2');
-  equal(obj.arr[2], '2', 'detach: should detach the advice'); // arr[2] would be '0' if it wasn't detached
+  t.equal(obj.arr[2], '2', 'detach: should detach the advice'); // arr[2] would be '0' if it wasn't detached
 
   advice.attach();
   obj.push('4');
-  equal(obj.arr[3], '0', 'attach: should put the advice back'); // arr[3] isn't '4' because '0' is pushed
+  t.equal(obj.arr[3], '0', 'attach: should put the advice back'); // arr[3] isn't '4' because '0' is pushed
+
+  t.end();
 });
 
 
-test('after should', function (){
+test('after should', function (t) {
   var obj = newObj(),
       x = false,
       context;
 
-  var advice = advise(obj).after('push', function (){
+  var advice = advise(obj).after('push', function () {
     obj.arr.push('advice');
     context = this;
   });
   obj.push('0');
 
-  equal(obj.arr[1], 'advice', 'after: should call advice after a method');
-  equal(obj, context, 'set the context to the object');
+  t.equal(obj.arr[1], 'advice', 'after: should call advice after a method');
+  t.equal(obj, context, 'set the context to the object');
 
   advice.detach();
   obj.push('2');
-  equal(obj.arr[2], '2', 'detach: should detach the advice');
+  t.equal(obj.arr[2], '2', 'detach: should detach the advice');
 
   advice.attach();
   obj.push('3');
-  equal(obj.arr[4], 'advice', 'attach: should attach the advice');
+  t.equal(obj.arr[4], 'advice', 'attach: should attach the advice');
+
+  t.end();
 });
 
-
-
-module('arguments');
-test('before should', function (){
+test('before should', function (t) {
   var obj = newObj(),
       _x = 0;
 
-  var setX = advise(obj).before('add', function (x, y){
+  var setX = advise(obj).before('add', function (x, y) {
     _x = x;
   });
   result = obj.add(2,3);
-  equal(_x, 2, 'pass in the arguments that were given to the method');
-  equal(result, 5, 'not mutate the arguments if nothing is returned');
+  t.equal(_x, 2, 'pass in the arguments that were given to the method');
+  t.equal(result, 5, 'not mutate the arguments if nothing is returned');
 
-  var doubleArgs = advise(obj).before('add', function (x, y){
+  var doubleArgs = advise(obj).before('add', function (x, y) {
     return [x * 2, y * 2];
   });
   result = obj.add(2, 3);
-  equal(result, 10, 'apply the mutated arguments if there is a return value');
+  t.equal(result, 10, 'apply the mutated arguments if there is a return value');
+
+  t.end();
 });
 
-test('after should', function (){
+test('after should', function (t) {
   var obj = newObj(),
       result = 0;
 
-  advise(obj).after('add', function (val){
+  advise(obj).after('add', function (val) {
     result = val;
   });
   obj.add(1,2);
-  equal(result, 3, 'send return value as first arguments');
+  t.equal(result, 3, 'send return value as first arguments');
+
+  t.end();
 });
 
-module('advice methods');
-test('once should', function (){
+test('once should', function (t) {
   var obj = newObj(),
       x = false,
       y = false,
       returns = false;
       advisor = advise(obj);
 
-  advisor.before('foo', function (){
+  advisor.before('foo', function () {
     x = true;
   }).once();
 
-  advisor.after('foo', function (){
+  advisor.after('foo', function () {
     y = true;
   }).once();
 
   returns = obj.foo(true);
 
-  ok(returns);
-  ok(x);
-  ok(y);
+  t.ok(returns);
+  t.ok(x);
+  t.ok(y);
   x = y = false;
 
   returns = obj.foo(true);
-  ok(!x, 'not be called a second time with before');
-  ok(!y, 'not be called a second time with after');
-  ok(returns, 'not mess up the original method');
+  t.ok(!x, 'not be called a second time with before');
+  t.ok(!y, 'not be called a second time with after');
+  t.ok(returns, 'not mess up the original method');
+
+  t.end();
 });
 
 /*
@@ -145,13 +153,13 @@ test('when should', function (){
   });
 
   obj.foo();
-  ok(!x, 'not call the advice if condition is false for before');
-  ok(!x, 'not call the advice if condition is false for after');
+  t.ok(!x, 'not call the advice if condition is false for before');
+  t.ok(!x, 'not call the advice if condition is false for after');
 
   a = b = true;
   obj.foo();
-  ok(x, 'call the advice when condition is true for before');
-  ok(y, 'call the advice when condition is true for after');
+  t.ok(x, 'call the advice when condition is true for before');
+  t.ok(y, 'call the advice when condition is true for after');
 
 });
 */
